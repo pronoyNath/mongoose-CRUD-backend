68f6077c2a0e2e885c82c9c88c1d709bb4fbd1b9
pronoyNath <pronoynath890@gmail.com>
2024-05-22 02:54:18 +0600

    order quantity validation added

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -5,39 +5,56 @@ import OrderZodSchema from "./order.validation";
 import { ProductModel } from "../product/product.model";
 
 const createOrder = async (req: Request, res: Response) => {
-  try {
-    const orderData: TOrder = req.body;
-
-    // Data validation using Zod
-    const zodParsedData = OrderZodSchema.parse(orderData);
-
-    // Check if the product exists
-    const product = await ProductModel.findById(zodParsedData.productId);
-
-    if (!product) {
-      return res.status(404).json({
+    try {
+      const orderData: TOrder = req.body;
+  
+      // Data validation using Zod
+      const zodParsedData = OrderZodSchema.parse(orderData);
+  
+      const product = await ProductModel.findById(zodParsedData.productId);
+  
+      if (!product) {
+        return res.status(404).json({
+          success: false,
+          message: "Product not found",
+        });
+      }
+  
+      // Check if the requested quantity is available
+      if (zodParsedData.quantity > product.inventory.quantity) {
+        return res.status(400).json({
+          success: false,
+          message: "Requested quantity exceeds available stock",
+        });
+      }
+      
+      
+      // Decrease the product quantity
+      product.inventory.quantity -= zodParsedData.quantity;
+      
+      // Update inStock status
+      product.inventory.inStock = product.inventory.quantity > 0;
+  
+      // Save the updated product
+      await product.save();
+  
+      // Call the service function to send this data
+      const result = await OrderServices.createOrderIntoDB(zodParsedData);
+  
+      return res.status(200).json({
+        success: true,
+        message: "Order is created successfully",
+        data: result,
+      });
+    } catch (error: any) {
+      return res.status(500).json({
         success: false,
-        message: "Product not found",
+        message: "Something went wrong",
+        error: error,
       });
     }
-
-    // Call the service function to send this data
-    const result = await OrderServices.createOrderIntoDB(zodParsedData);
-
-    // Send response
-    res.status(200).json({
-      success: true,
-      message: "Order is created successfully",
-      data: result,
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: "Something went wrong",
-      error: err,
-    });
-  }
-};
+  };
+  
 
 const getAllOrders = async (req: Request, res: Response) => {
   try {
